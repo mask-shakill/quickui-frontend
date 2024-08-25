e004287c90ab211f263383c25edd31889f92445f
Shakil <mask.shakill@gmail.com>
2024-08-25 18:17:16 +0600

    added inital setup

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import LandingPage from "@/pages/landing-page/LandingPage";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+};
+
+export default Home;
